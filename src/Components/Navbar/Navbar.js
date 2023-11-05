fbba5d5c671199757f5ea00a2849d83acc1ef2f8
AmmarA944 <149663087+AmmarA944@users.noreply.github.com>
2023-11-05 09:39:12 +0200

    Navbar modification

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,14 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
 export const Navbar = () => {
   const [isUserSignedIn, setIsUserSignedIn] = useState(false);
   const [userType, setUserType] = useState('user');
+  const [companyData, setCompanyData] = useState({ logoUrl: '', name: '' });
+
+  useEffect(() => {
+    // Fetlogo,name api
+    fetchCompanyData();
+  }, []);
+
+  const fetchCompanyData = async () => {
+    try {
+      const response = await fetch('/api/'); 
+      if (response.ok) {
+        const data = await response.json();
+        setCompanyData({ logoUrl: data.logoUrl, name: data.name });
+      } else {
+        console.error('Failed to fetch company data');
+      }
+    } catch (error) {
+      console.error('Error fetching company data:', error);
+    }
+  };
 
   return (
     <>
       <header className='Navbar'>
+        <div className="logo-container">
+          <img src={companyData.logoUrl} alt="Logo" id="logo" />
+          <span id="company-name">{companyData.name}</span>
+        </div>
         <ul>
           <li><NavLink id='nav' to="/">Home</NavLink></li>
           <li><NavLink id='nav' to="/services">Services</NavLink></li>
